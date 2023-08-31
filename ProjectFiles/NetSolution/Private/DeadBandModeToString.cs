67ddf1fb16c017e92aff2c3c695adb9eb17b897b
soseguera <129338553+soseguera@users.noreply.github.com>
2023-08-31 12:57:38 -0700

    Add files via upload

---
+++ ProjectFiles/NetSolution/Private/DeadBandModeToString.cs
@@ -0,0 +1,23 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+[MapType(NamespaceUri = "PushAgentDemo", Guid = "d8bd930dcaeadb6e50d007a6864ac63d")]
+public class DeadBandModeToString : FTOptix.CoreBase.ValueMapConverter
+{
+#region Children properties
+    //-------------------------------------------
+    // WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+    //-------------------------------------------
+    public UAManagedCore.IUAObject Pairs
+    {
+        get
+        {
+            return (UAManagedCore.IUAObject)Refs.GetObject("Pairs");
+        }
+    }
+#endregion
+}
---
+++ ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
@@ -0,0 +1,16 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+[MapType(NamespaceUri = "PushAgentDemo", Guid = "ba3d92757e215f7e6b4c8b62cbc01d02")]
+public class MainWindow : FTOptix.UI.Window
+{
+}
+
+[MapType(NamespaceUri = "PushAgentDemo", Guid = "0c09787cb9b244a6747c90f1c3874789")]
+public class VarPanel : FTOptix.UI.Panel
+{
+}
